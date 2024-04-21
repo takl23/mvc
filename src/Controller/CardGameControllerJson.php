966674469837d6805752ae8b128ec8cb5a74d850
Tamara <takl23@student.bth.se>
2024-04-21 19:41:46 +0200

    Add option to draw one or more cards from deck

--- src/Controller/CardGameControllerJson.php
+++ src/Controller/CardGameControllerJson.php
@@ -7,10 +7,13 @@ use App\Card\DeckOfCards;
 use App\Card\CardHand;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Exception;
 
 date_default_timezone_set("Europe/Stockholm");
 
@@ -78,9 +81,124 @@ class CardGameControllerJson extends AbstractController
         return $response;
     }
 
-    // #[Route("/api/deck/draw", name: "draw", methods: ["POST"])]
-    // public function jsonDraw(SessionInterface $session): Response
-    // {
-       
-    // }
+    #[Route("/api/deck/draw", name: "draw", methods: ["POST"])]
+    public function jsonDraw(SessionInterface $session): Response
+    {
+        $deck = $session->get("cards_left_in_deck");
+
+        if ($deck === null) {
+            $deck = new DeckOfCards();
+            $deck->shuffle();
+            $session->set("cards_left_in_deck", $deck);
+        }
+
+        $hand = new CardHand();
+        $drawnCard = $deck->draw();
+
+        // Om det inte finns några kort kvar i kortleken, visa ett meddelande
+        if ($drawnCard === null) {
+        return new Response("The deck is empty!");
+        }
+        
+        $hand->add($drawnCard);
+
+        // Store the hand in the session
+        $session->set("drawn_cards", $hand->getHand());
+
+        // Hämta kortleken som en array av kortrepresentationer
+        $deckLeft = [];
+        foreach ($deck->getDeck() as $card) {
+            $deckLeft[] = $card->representCard();
+        }
+
+        $deckDrawn = [];
+        foreach ($hand->getHand() as $card) {
+        $deckDrawn[] = $card->representCard();
+        }
+
+
+        $deckLeftInt = $deck->countDeck();
+    
+
+        $data = [
+            "cardsLeftInt" => $deckLeftInt,
+            "cardsDrawn" => $deckDrawn,
+            "cardsLeft" => $deckLeft,           
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api/deck/draw/{number<\d+>?}", name: "drawmany", methods: ["POST"])]
+    public function jsonDrawMany(SessionInterface $session, Request $request): Response
+    {        
+
+   // Hämta värdet för number från requesten
+   $number = $request->request->getInt('number', 1); // Använd det skickade värdet för antalet kort
+
+
+        $deck = $session->get("cards_left_in_deck");
+
+        if ($deck === null) {
+            $deck = new DeckOfCards();
+            $deck->shuffle();
+            $session->set("cards_left_in_deck", $deck);
+        }
+
+        if ($number > 52) {
+            throw new Exception("Can not draw more cards then the number of cards a deck contains");
+        } elseif ($number > $deck->countDeck()) {
+            throw new Exception("Can not draw more cards than cards left in deck.");
+        }
+
+        $hand = new CardHand();
+        
+        for ($i = 1; $i <= $number; $i++) {
+            $drawnCard = $deck->draw();
+        
+            // Lägg till det dragna kortet till handen
+            if ($drawnCard !== null) {
+                $hand->add($drawnCard);
+            } else {
+                break; // Om kortleken är tom, bryt loopen
+            }
+        }
+    
+        // Store the hand in the session
+        $session->set("drawn_cards", $hand->getHand());
+
+          // Uppdatera kortleken i sessionen
+            $session->set("cards_left_in_deck", $deck);
+        
+        // Hämta kortleken som en array av kortrepresentationer
+        $deckLeft = [];
+        foreach ($deck->getDeck() as $card) {
+            $deckLeft[] = $card->representCard();
+        }
+
+        $deckDrawn = [];
+        foreach ($hand->getHand() as $card) {
+        $deckDrawn[] = $card->representCard();
+        }
+
+
+        $deckLeftInt = $deck->countDeck();
+    
+
+        $data = [
+            "cardsLeftInt" => $deckLeftInt,
+            "cardsDrawn" => $deckDrawn,
+            "cardsLeft" => $deckLeft,           
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
 }
