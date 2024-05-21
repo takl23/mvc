c1c2550f95038065b907f9dae7f411d72a4dee74
Tamara <takl23@student.bth.se>
2024-05-21 20:55:08 +0200

    Add phpunit test for class Card

---
+++ tests/Card/CardTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Card;
+
+use PHPUnit\Framework\TestCase;
+use Exception;
+
+/**
+ * Test cases for class Card.
+ */
+class CardTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties, use valid arguments for card creation.
+     */
+    public function testCreateCard()
+    {
+        $card = new Card(Card::KING, Card::HEARTS);
+        $this->assertInstanceOf("\App\Card\Card", $card);
+        
+        $res = $card->representCard();
+        $this->assertEquals('K ♥', $res);
+
+
+    }
+
+    /**
+     * Test to create a card with invalid value
+     */
+    public function testInvalidCardValue()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Value must be between 1 and 13.");
+        new Card(0, Card::HEARTS);
+    }
+
+    /**
+     * Test to create a card with invalid suit
+     */
+    public function testInvalidCardSuit()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage("Invalid suit.");
+        new Card(1, 5);
+    }
+
+    /**
+     * Test color of a card
+     */
+    public function testCardColor()
+    {
+        $card = new Card(Card::KING, Card::HEARTS);
+        $this->assertInstanceOf("\App\Card\Card", $card);
+        $this->assertEquals('red', $card->getColor());
+    }
+    
+}
